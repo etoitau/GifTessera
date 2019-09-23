529ff83d1845ad3681d8ef2f7a832287dacc8b75
Kyle Chatman <kchatman@gmail.com>
2019-09-23 10:45:34 -0700

    finished

--- app/src/main/java/com/etoitau/giftessera/MainActivity.kt
+++ app/src/main/java/com/etoitau/giftessera/MainActivity.kt
@@ -16,7 +16,6 @@ import android.util.DisplayMetrics
 import android.util.Log
 import android.view.*
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
@@ -35,8 +34,6 @@ import java.nio.charset.StandardCharsets
 class MainActivity : AppCompatActivity() {
     lateinit var drawSession: DrawSession   // object managing current animation project
     var isPeeking = false                   // currently in peek view mode
-    private lateinit var toast: Toast               // reusable toast object
-    private var toastOffset: Int = 50
     private lateinit var paletteManager: PaletteManager // object for managing library of palettes
     private lateinit var breadBox: BreadBox
 
@@ -53,11 +50,6 @@ class MainActivity : AppCompatActivity() {
         const val CODE_LOAD = 5
         const val CODE_FILE = 7
         const val CODE_PERM = 9
-
-        // bread durations
-        const val SHORT = 1000L
-        const val MEDIUM = 1500L
-        const val LONG = 2000L
     }
 
 
@@ -78,7 +70,7 @@ class MainActivity : AppCompatActivity() {
                 recoveredID = savedInstanceState.getInt(SAVE_ID)
                 recoveredName = savedInstanceState.getString(SAVE_NAME)
                 wasPortrait = savedInstanceState.getBoolean(SAVE_IS_PORTRAIT)
-                val savedStripString = savedInstanceState.getString(Companion.SAVE_STRIP)
+                val savedStripString = savedInstanceState.getString(SAVE_STRIP)
                 recoveredIndex = savedInstanceState.getInt(SAVE_FRAME_NUMBER)
                 if (savedStripString != null) {
                     recoveredFilmstrip = toFilmstrip(savedStripString.toByteArray(StandardCharsets.ISO_8859_1))
@@ -90,11 +82,7 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
 
-
-        // initialize toast for use by showToast
-//        toast = Toast.makeText(this, "", Toast.LENGTH_LONG)
-//        toastOffset = toast.yOffset * 2
-//        toast.setGravity(Gravity.BOTTOM, 0, toastOffset)
+        // get an object for managing the notification textbox (DIY Toast)
         breadBox = BreadBox(bread)
 
         // initialize drawingBoard with display metrics
@@ -147,7 +135,7 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle?) {
         try{
             outState?.run {
-                putString(Companion.SAVE_STRIP, String(toByte(drawSession.filmStrip)!!, StandardCharsets.ISO_8859_1))
+                putString(SAVE_STRIP, String(toByte(drawSession.filmStrip)!!, StandardCharsets.ISO_8859_1))
                 putInt(SAVE_FRAME_NUMBER, drawSession.filmIndex)
                 putBoolean(SAVE_IS_PORTRAIT, drawingBoard.isPortrait)
                 if (drawSession.saveId != null && drawSession.saveName != null) {
@@ -194,8 +182,7 @@ class MainActivity : AppCompatActivity() {
         drawSession.addInsertFrame()
         updateTitle()
         breadBox.setMessage(String.format(getString(R.string.frame_added_after), drawSession.filmIndex))
-            .showFor(MEDIUM)
-//        showToast(String.format(getString(R.string.frame_added_after), drawSession.filmIndex), false)
+            .showFor(BreadBox.MEDIUM)
     }
 
     /**
@@ -293,44 +280,11 @@ class MainActivity : AppCompatActivity() {
             R.id.menuHelp -> showHelp()
             R.id.menuAbout -> showAbout()
             else -> {
-                breadBox.setMessage(resources.getString(R.string.invalid_selection)).showFor(MEDIUM)
-//                showToast(resources.getString(R.string.invalid_selection), true)
+                breadBox.setMessage(resources.getString(R.string.invalid_selection)).showFor(BreadBox.MEDIUM)
             }
         }
         return status
     }
-
-    /**
-     * modifies and shows a pre-made toast with provided message
-     * if there are many incoming messages they will not pile up, but
-     * later messages may not show because android forces the toast to fade out and not
-     * come back for a bit
-     * @param message
-     */
-//    private fun showToast(message: String) {
-//        toast.setText(message)
-//        toast.show()
-//    }
-
-    /**
-     * if low priority, uses showToast above
-     * if high priority, creates new toast and adds to queue so it will definitely get shown
-     */
-//    fun showToast(message: String, isPriority: Boolean) {
-//        if (isPriority) {
-//            val newToast = Toast.makeText(this, message, Toast.LENGTH_LONG)
-//            newToast.setGravity(Gravity.BOTTOM, 0, toastOffset)
-//            newToast.show()
-//        } else {
-//            showToast(message)
-//        }
-//    }
-
-//    inner class Toaster(private val message: String, private val isPriority: Boolean): Runnable {
-//        override fun run() {
-//            showToast(message, isPriority)
-//        }
-//    }
 
     /**
      * "Save" menu item selected
@@ -357,11 +311,9 @@ class MainActivity : AppCompatActivity() {
                 // update file in database
                 dbHelper.updateFile(dbFile)
                 breadBox.setMessage(getString(R.string.saved_as) + " " + drawSession.saveName)
-                    .showFor(LONG)
-//                showToast(getString(R.string.saved_as) + " " + drawSession.saveName, true)
+                    .showFor(BreadBox.LONG)
             } catch (e: Exception) {
-                breadBox.setMessage(resources.getString(R.string.error_saving)).showFor(LONG)
-//                showToast(resources.getString(R.string.error_saving), true)
+                breadBox.setMessage(resources.getString(R.string.error_saving)).showFor(BreadBox.LONG)
             }
         }
     }
@@ -402,8 +354,7 @@ class MainActivity : AppCompatActivity() {
             val name = data.getStringExtra("name")
             val byteArray = data.getByteArrayExtra("file")
             if (name == null || byteArray == null) {
-                breadBox.setMessage(getString(R.string.error_saving)).showFor(LONG)
-//                showToast(getString(R.string.error_saving), true)
+                breadBox.setMessage(getString(R.string.error_saving)).showFor(BreadBox.LONG)
             } else {
                 drawSession.loadDataBaseFile(DatabaseFile(id, name, byteArray))
                 updateTitle(1)
@@ -436,13 +387,11 @@ class MainActivity : AppCompatActivity() {
     private fun exportGif() {
         if (drawSession.saveName == null) {
             // check saved locally first (so it has name)
-            breadBox.setMessage(getString(R.string.save_before_export)).showFor(LONG)
-//            showToast(getString(R.string.save_before_export), true)
+            breadBox.setMessage(getString(R.string.save_before_export)).showFor(BreadBox.LONG)
             return
         } else if (!isReadyToSave()){
             // check file system is available
-            breadBox.setMessage(getString(R.string.fs_not_available)).showFor(LONG)
-//            showToast(getString(R.string.fs_not_available), true)
+            breadBox.setMessage(getString(R.string.fs_not_available)).showFor(BreadBox.LONG)
             return
         }
 
@@ -480,8 +429,7 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
-                    breadBox.setMessage(getString(R.string.permission_required)).showFor(LONG)
-//                    showToast(getString(R.string.permission_required), true)
+                    breadBox.setMessage(getString(R.string.permission_required)).showFor(BreadBox.LONG)
                 }
                 return
             }
@@ -508,9 +456,10 @@ class MainActivity : AppCompatActivity() {
                 copyStrip.add(frame.copy(frame.config, true))
 
             // let user know save is starting in background
-            breadBox.setMessage(String.format(getString(R.string.saving_to_gif), fName))
-                .showFor(MEDIUM)
-//            runOnUiThread(Toaster(String.format(getString(R.string.saving_to_gif), fName), true))
+            runOnUiThread(
+                BreadBox.RunBread(breadBox,
+                    String.format(getString(R.string.saving_to_gif), fName),
+                    BreadBox.MEDIUM))
 
             // ByteArray to put gif in
             val gifByteArray: ByteArray
@@ -527,10 +476,10 @@ class MainActivity : AppCompatActivity() {
                 val nFrames = copyStrip.size
                 for (i in 0 until nFrames) {
                     // notify progress
-                    breadBox
-                        .setMessage(String.format(getString(R.string.processing_frame), i + 1, nFrames + 1))
-                        .showFor(SHORT)
-//                    runOnUiThread(Toaster(String.format(getString(R.string.processing_frame), i + 1, nFrames + 1), false))
+                    runOnUiThread(BreadBox.RunBread(
+                        breadBox,
+                        String.format(getString(R.string.processing_frame), i + 1, nFrames),
+                        BreadBox.SHORT))
                     e.addFrame(Bitmap.createScaledBitmap(copyStrip[i],
                         copyStrip[i].width * drawingBoard.xScale,
                         copyStrip[i].height * drawingBoard.yScale, false))
@@ -556,17 +505,15 @@ class MainActivity : AppCompatActivity() {
 
         // let user know save is complete
         override fun onPostExecute(result: Int?) {
+            var msg: String
             if (result == 1) {
-                breadBox.setMessage(String.format(getString(R.string.finished_saving_gif), fName))
-                    .showFor(LONG)
-//                runOnUiThread(Toaster(String.format(getString(R.string.finished_saving_gif), fName), true))
+                msg = String.format(getString(R.string.finished_saving_gif), fName)
                 Log.i("Async finished", "gif saved")
             } else {
-                breadBox.setMessage(String.format(getString(R.string.error_saving_gif), fName))
-                    .showFor(LONG)
-//                runOnUiThread(Toaster(String.format(getString(R.string.error_saving_gif), fName), true))
+                msg = String.format(getString(R.string.error_saving_gif), fName)
                 Log.i("Async finished", "error")
             }
+            runOnUiThread(BreadBox.RunBread(breadBox, msg, BreadBox.LONG))
         }
     }
 
--- app/src/main/java/com/etoitau/giftessera/domain/BreadBox.kt
+++ app/src/main/java/com/etoitau/giftessera/domain/BreadBox.kt
@@ -6,35 +6,59 @@ import android.os.CountDownTimer
 import android.view.View
 import android.widget.TextView
 
+/**
+ * Gives a TextView functionality similar to a Toast,
+ * but you can keep updating and extending time as needed
+ *
+ */
 class BreadBox constructor(val textView: TextView) {
+    companion object {
+        // bread durations
+        const val SHORT = 1000L
+        const val MEDIUM = 1500L
+        const val LONG = 2000L
+
+        // default transparency
+        const val ALPHA: Float = 0.8f
+
+        // animation timing
+        const val POPUP_TIME: Long = 300
+        const val FADE_TIME: Long = 500
+    }
+
+    // timer for how long message should display
     private lateinit var timer: CountDownTimer
     private var isTimerRunning = false
-    private val alpha: Float = 0.8f
-    private val popUpTime: Long = 300
-    private val fadeTime: Long = 500
 
+    // Animations
+    // message appears
     private val up: ObjectAnimator =
         ObjectAnimator.ofFloat(textView, "translationY", 200f, 0f)
     private val fadeIn =
-        ObjectAnimator.ofFloat(textView, "alpha", 0f, alpha)
+        ObjectAnimator.ofFloat(textView, "alpha", 0f, ALPHA)
     private val popUpSet = AnimatorSet()
-
-    private val fadeOut = ObjectAnimator.ofFloat(textView, "alpha", alpha, 0f)
+    // message fades out
+    private val fadeOut = ObjectAnimator.ofFloat(textView, "alpha", ALPHA, 0f)
 
     init {
         textView.text = ""
+        // finish setting up animations at init
         popUpSet.play(up).with(fadeIn)
-        popUpSet.duration = popUpTime
-
-        fadeOut.duration = fadeTime
+        popUpSet.duration = POPUP_TIME
+        fadeOut.duration = FADE_TIME
     }
 
+    // set the message to display, doesn't show until showFor is called, which can be fluently called
     fun setMessage(msg: String): BreadBox {
         textView.text = msg
         return this
     }
 
+
+     // Make the message viable for desired duration
     fun showFor(milliseconds: Long) {
+         // cancel any current timer to replace with new,
+         // make sure isTimerRunning is true after this call
         if (isTimerRunning) {
             timer.cancel()
         } else {
@@ -46,21 +70,37 @@ class BreadBox constructor(val textView: TextView) {
             textView.visibility = View.VISIBLE
             popUpSet.start()
         } else if (fadeOut.isRunning) {
+            // if currently fading out, stop that and bring back
             fadeOut.cancel()
             textView.visibility = View.VISIBLE
-            textView.alpha = alpha
+            textView.alpha = ALPHA
         }
+         // set timer to hide message after specified time
         timer = setTimer(milliseconds).start()
     }
 
+    // Sometimes need to execute as a runnable
+    class RunBread(
+        private val breadBox: BreadBox,
+        private val message: String,
+        private val milliseconds: Long): Runnable {
+
+        override fun run() {
+            breadBox.setMessage(message).showFor(milliseconds)
+        }
+    }
+
+    // hide message after given time
     private fun setTimer(forMillis: Long): CountDownTimer {
 
-        return object : CountDownTimer(forMillis + fadeTime, forMillis) {
+        return object : CountDownTimer(forMillis + FADE_TIME, forMillis) {
+            // set to tick when it's time to start fading out, but ignore immediate tick
             override fun onTick(remaining: Long) {
-                if (remaining <= fadeTime)
+                if (remaining <= FADE_TIME)
                     fadeOut.start()
             }
 
+            // hide entirely, fade out should be complete by now
             override fun onFinish() {
                 textView.visibility = View.GONE
                 isTimerRunning = false
